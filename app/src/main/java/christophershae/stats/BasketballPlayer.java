c32dbaefa08b06620c0f617723641130a7c77531
Christopher Smith <chshsmit@ucsc.edu>
2017-07-18 11:28:36 -0700

    Basketball: Stat listing table

---
+++ app/src/main/java/christophershae/stats/BasketballPlayer.java
@@ -0,0 +1,8 @@
+package christophershae.stats;
+
+/**
+ * Created by chrissmith on 7/18/17.
+ */
+
+public class BasketballPlayer {
+}
---
+++ app/src/main/java/christophershae/stats/TableActivity.java
@@ -0,0 +1,13 @@
+package christophershae.stats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table);
+    }
+}
