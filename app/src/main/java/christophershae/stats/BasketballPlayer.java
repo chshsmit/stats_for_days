81b2c6dd897d29536344af87867efc7e1e4989dc
Christopher Smith <chshsmit@ucsc.edu>
2017-07-19 16:28:27 -0700

    Basketball: Buttons now update stats in the hash table

--- app/src/main/java/christophershae/stats/BasketballPlayer.java
+++ app/src/main/java/christophershae/stats/BasketballPlayer.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 public class BasketballPlayer {
 
-    final public String playerName = new String();
+    public String playerName = new String();
 
     //Creates string keys for all stats and declare a hashtable to store them
     List<String> statKeys = new ArrayList<String>(Arrays.asList("2PM", "2PA", "3PM","3PA","AST","PASS","OREB","DREB","BLK","STL","FTM","FTA","TO","FOUL"));
@@ -21,14 +21,47 @@ public class BasketballPlayer {
     //Constructor for Basketball player class
     public BasketballPlayer(){
         //Initialize all the player's stats to 0
-        for(String stat : statKeys){
-            playerStats.put(stat, 0);
+        for(String stat : this.statKeys){
+            this.playerStats.put(stat, 0);
+            System.out.println(stat+":" +playerStats.get(stat));
         }
     }
 
     //Padding those stats
     public void increaseStat(String statKey){
         int oldStat = playerStats.get(statKey);
-        playerStats.put(statKey, ++oldStat);
+        System.out.println("You are updating this stat: " +statKey);
+
+        if(statKey.equals("2PM")){
+            playerStats.put(statKey, ++oldStat);
+            oldStat = playerStats.get("2PA");
+            playerStats.put("2PA", ++oldStat);
+        }else if(statKey.equals("3PM")){
+            playerStats.put(statKey, ++oldStat);
+            oldStat = playerStats.get("3PA");
+            playerStats.put("3PA", ++oldStat);
+        }else if(statKey.equals("FTM")){
+            playerStats.put(statKey, ++oldStat);
+            oldStat = playerStats.get("FTA");
+            playerStats.put("FTA", ++oldStat);
+        }else {
+            playerStats.put(statKey, ++oldStat);
+        }
+
     }
+
+    //Sets the players name
+    public void setPlayerName(String name){
+        this.playerName = name;
+    }
+
+
+    //Prints out all the stats
+    public void showStats(){
+        for(String stat: this.statKeys){
+            System.out.println(stat+":" +playerStats.get(stat));
+        }
+    }
+
+
 }
--- app/src/main/java/christophershae/stats/TableActivity.java
+++ app/src/main/java/christophershae/stats/TableActivity.java
@@ -2,6 +2,8 @@ package christophershae.stats;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TableActivity extends AppCompatActivity {
 
@@ -10,6 +12,15 @@ public class TableActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table);
     }
+
+    BasketballPlayer myPlayer = new BasketballPlayer();
+
+    public void addStat(View v){
+        String statKey = ((Button) v).getText().toString();
+        myPlayer.increaseStat(statKey);
+        myPlayer.showStats();
+    }
+
 
 
 }
