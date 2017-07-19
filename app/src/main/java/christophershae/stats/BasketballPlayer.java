1ec0e57d6d0e47bf5af7b2866d6379e86e2bab65
Christopher Smith <chshsmit@ucsc.edu>
2017-07-18 20:20:58 -0700

    Basketball: Prototype layout and beginning player class

--- app/src/main/java/christophershae/stats/BasketballPlayer.java
+++ app/src/main/java/christophershae/stats/BasketballPlayer.java
@@ -1,8 +1,34 @@
 package christophershae.stats;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by chrissmith on 7/18/17.
  */
 
 public class BasketballPlayer {
+
+    final public String playerName = new String();
+
+    //Creates string keys for all stats and declare a hashtable to store them
+    List<String> statKeys = new ArrayList<String>(Arrays.asList("2PM", "2PA", "3PM","3PA","AST","PASS","OREB","DREB","BLK","STL","FTM","FTA","TO","FOUL"));
+    Map<String, Integer> playerStats = new HashMap<String, Integer>();
+
+    //Constructor for Basketball player class
+    public BasketballPlayer(){
+        //Initialize all the player's stats to 0
+        for(String stat : statKeys){
+            playerStats.put(stat, 0);
+        }
+    }
+
+    //Padding those stats
+    public void increaseStat(String statKey){
+        int oldStat = playerStats.get(statKey);
+        playerStats.put(statKey, ++oldStat);
+    }
 }
